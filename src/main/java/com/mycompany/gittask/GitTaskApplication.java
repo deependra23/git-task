ae26af0e7e9ec3516ab9b5309bb7c682cc917dcd
Deependra <deependrapoudel19@gmail.com>
2021-08-29 21:01:15 +1000

    initital phase

---
+++ src/main/java/com/mycompany/gittask/GitTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.gittask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/gittask/GitTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.gittask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
