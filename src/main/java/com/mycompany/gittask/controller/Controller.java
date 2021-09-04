497d17e3388c240fa50c578193e4322cfc61595e
Deependra <deependrapoudel19@gmail.com>
2021-09-04 18:47:10 +1000

    Adding REST API ON GITHUB REMOTE

--- src/main/java/com/mycompany/gittask/controller/Controller.java
+++ src/main/java/com/mycompany/gittask/controller/Controller.java
@@ -1,15 +1,22 @@
-package com.mycompany.gittask;
+package com.mycompany.gittask.controller;
 
+import com.mycompany.gittask.model.AuthorList;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class Controller {
-    @RequestMapping("/message")
-    public String getMessage(){
-    return "Hello People";
-   }
+    @RequestMapping("/author")
+    public List<AuthorList> getAuthorList(){
+        return Arrays.asList(
+                new AuthorList(989095, "Deependra", "Sydney"),
+                new AuthorList(988339, "Nandeshwor", "Denver")
+        );
+    }
+
 
 }
